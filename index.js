caab0f54c8bbe659b5a247288b0a9b0c0664803e
Zenande Mbane <87269989+Averetech@users.noreply.github.com>
2024-09-19 14:34:31 +0200

    added axios, updated HTML, CSS, and JS

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-function showAdvice(event) {
-  let advice = document.querySelector("#advice");
-  event.preventDefault();
+function showAnswer(response) {
   new Typewriter("#advice", {
-    strings: "Help me write an application for a job...",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 10,
     cursor: null,
   });
 }
-//let submitForm = document.querySelector("#zenn-bot-form");
-window.addEventListener("load", showAdvice);
+
+function getApiInfo(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+  let apiKey = "1e34ff4f3f045a566c8e39at1b7beo2f";
+  let prompt = `Please give your answer based on what the user is asking, ${userInput.value}`;
+  let context =
+    "You are an AI assistant that is very smart and give informatiom about any topic you are asked. Provide the user with the information that they want. Please be clear and CONCISE! And add 'Zenn.' inside a <strong></strong> element at the end of the answer.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showAnswer);
+}
+
+let submitForm = document.querySelector("#zenn-bot-form");
+submitForm.addEventListener("submit", getApiInfo);
